4971bccfb2312be6783e7d06b66455103dbcf512
Derrick DeConti <deconti.d@gmail.com>
2017-05-24 00:56:53 -0400

    Inverted sam map; demux left to handle.

--- demux_chimeras.py
+++ demux_chimeras.py
@@ -6,19 +6,34 @@ Demuxes miR from chimeric CLEAR-CLIP reads.
 
 import argparse
 import pysam
+from Bio import SeqIO
+from collections import defaultdict
+
+
+def demux_reads(fastq, inverted_map):
+    '''
+    Removes chimeric miR sequence from read.
+    '''
+    for record in seqIO.parse(open(fastq, 'rU'), "fastq"):
+        if record.id not in inverted_map:
+            continue
+        demuxed_read = None
+        yield demuxed_read
 
 
 def reverse_map_bam(bam):
     '''
     Reverse maps the miR in BAM to FASTQ headers.
-    ''' 
-    samfile = pysam.AlignmentFile(bam, 'rb')
-    for read in samfile.fetch():
-        print read.reference_id
-        pass
-    samfile.close()
-    return
-    pass
+    '''
+    inverted_map = defaultdict(list)
+    with pysam.AlignmentFile(bam, 'r') as samfile:
+        for read in samfile.fetch():
+            if read.is_unmapped:
+                continue
+            inverted_map[read.reference_name].append((read.query_name,
+                                                      read.pos))
+            #inverted_map[read.reference_id].append((read.refereancename, read.pos))
+    return inverted_map
 
 
 def main():
@@ -33,6 +48,11 @@ def main():
                         help="FASTQ of the chimeric reads")
     args = parser.parse_args()
     bam2fq_map = reverse_map_bam(args.BAM)
+    for k in bam2fq_map:
+        print k
+        for v in bam2fq_map[k]:
+            print '\t', v
+    demux_reads(bam2fq_map, args.FASTQ)
 
 if __name__ == "__main__":
     main()
--- interleave_fq.py
+++ interleave_fq.py
@@ -1,25 +1,58 @@
-#This Python script requires Biopython 1.51 or later
-from Bio import SeqIO
+#!/usr/bin/python
+
+'''
+Interleaves paired fastq reads.
+'''
+
+import argparse
 import itertools
+import sys
+from Bio import SeqIO
+from StringIO import StringIO
 
-#Setup variables (could parse command line args instead)
-file_f = "SRR001666_1.fastq"
-file_r = "SRR001666_2.fastq"
-file_out = "SRR001666_interleaved.fastq"
-format = "fastq" #or "fastq-illumina", or "fasta", or ...
 
-def interleave(iter1, iter2) :
-    for (forward, reverse) in itertools.izip(iter1,iter2):
+def interleave(iter1, iter2):
+    '''
+    Interleaves fastq
+    '''
+    for (forward, reverse) in itertools.izip(iter1, iter2):
         assert forward.id == reverse.id
         forward.id += "/1"
         reverse.id += "/2"
         yield forward
         yield reverse
 
-records_f = SeqIO.parse(open(file_f,"rU"), format)
-records_r = SeqIO.parse(open(file_r,"rU"), format)
 
-handle = open(file_out, "w")
-count = SeqIO.write(interleave(records_f, records_r), handle, format)
-handle.close()
-print "%i records written to %s" % (count, file_out)
+def main():
+    '''
+    Arg parsing and central dispatch.
+    '''
+    # Arg parsing
+    parser = argparse.ArgumentParser(description="Interleave paired fastq.")
+    parser.add_argument("first", metavar="FIRST_FASTQ",
+                        help="first in fastq pair")
+    parser.add_argument("second", metavar="SECOND_FASTQ",
+                        help="second in fastq pair")
+    parser.add_argument("-n", "--name", metavar="OUTPUT_NAME",
+                        default=None,
+                        help="name of output fastq [default: stdout]")
+    parser.add_argument("-f", "--format",
+                        default="fastq",
+                        help="format of files [default: fastq]")
+    args = parser.parse_args()
+    # function dispatching
+    records_f = SeqIO.parse(open(args.first, 'rU'), args.format)
+    records_r = SeqIO.parse(open(args.second, 'rU'), args.format)
+    if args.name:
+        with open(args.name, 'w') as handle:
+            SeqIO.write(interleave(records_f, records_r), handle, args.format)
+    else:
+        for record in interleave(records_f, records_r):
+            out_handle = StringIO()
+            SeqIO.write(record, out_handle, args.format)
+            data = out_handle.getvalue()
+            sys.stdout.write(data)
+
+
+if __name__ == "__main__":
+    main()
